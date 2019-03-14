5edf9ef8df2a8aec8a3b2f5fae776c7896fac8ab
PoonamGhutukade <poonam.ghutukade@gmail.com>
2019-03-14 17:44:38 +0530

    Clear Terminal

---
+++ week1/ClearTerminal_28.py
@@ -0,0 +1,12 @@
+""" 28. Write a Python program to clear the screen or terminal. """
+
+import os
+import time
+
+print("hello")
+time.sleep(2)
+os.system("clear")
+
+"""TERM environment variable not set. If this error is occurred then , Go to ->run ,click on ->Edit configuration
+ and select Emulate terminal in output configuration """
+
