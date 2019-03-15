977ad27ce64366f01cc7eda2a4b406be015b5da6
PoonamGhutukade <poonam.ghutukade@gmail.com>
2019-03-15 13:14:26 +0530

    Inheritance - parent class

---
+++ Week3/Linear Algebra/ParentMatrix.py
@@ -0,0 +1,14 @@
+# here we are using single inheritance
+class Parent:
+    # constructor used for declaring matrix
+    def __init__(self):
+        self.matrix1 = [[1, 7, 3],
+                        [4, 5, 6],
+                        [7, 8, 9]]
+
+        self.matrix2 = [[5, 8, 1],
+                        [4, 5, 6],
+                        [7, 8, 9]]
+
+
+#obj = Parent()
