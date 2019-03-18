e97f17207c905d9b3d364923f1743f790e48b8f2
PoonamGhutukade <poonam.ghutukade@gmail.com>
2019-03-18 10:49:43 +0530

    default z values

---
+++ Week3/Probability_Statistics/Prob_Z_values.py
@@ -0,0 +1,13 @@
+class StoreValues:
+    def __init__(self):
+        # default +ve z value, z(1) = 0.8413
+        self.z_one = 0.8413
+        # default +ve z value, z(2.5) = 0.9938
+        self.z_first = 0.9938
+        # default -ve z value, z(-2.25) = 0.0122
+        self.z_second = 0.0122
+        # default +ve z value, z(1.25) = 0.8944
+        self.z_third = 0.8944
+
+
+
