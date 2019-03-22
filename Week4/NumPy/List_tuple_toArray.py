0cf9a51e533400466b831615347b9a53baf63a99
PoonamGhutukade <poonam.ghutukade@gmail.com>
2019-03-22 19:47:23 +0530

    convert a list and tuple into arrays

--- Week4/NumPy/List_tuple_toArray.py
+++ Week4/NumPy/List_tuple_toArray.py
@@ -1,9 +1,4 @@
-"""8. Write a Python program to convert a list and tuple into arrays.
-List to array:
-[1 2 3 4 5 6 7 8]
-Tuple to array:
-[[8 4 6]
-[1 2 3]]"""
+"""8. Write a Python program to convert a list and tuple into arrays."""
 
 from Week4.Utility.Util import UtilClass
 import numpy as np
