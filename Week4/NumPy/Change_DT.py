d8cf255756b8943c00b2073c935fa0d0c3b478c2
PoonamGhutukade <poonam.ghutukade@gmail.com>
2019-03-22 19:45:51 +0530

    change the data type of an array

--- Week4/NumPy/Change_DT.py
+++ Week4/NumPy/Change_DT.py
@@ -2,14 +2,7 @@ import numpy as np
 import re
 from Week4.Utility.Util import UtilClass
 
-"""17. Write a Python program to change the data type of an array. 
-Expected Output:
-[[ 2 4 6]
-[ 6 8 10]] 
-Data type of the array x is: int32 
-New Type: float64 
-[[ 2. 4. 6.] 
-[ 6. 8. 10.]]"""
+"""17. Write a Python program to change the data type of an array. """
 
 
 class ChangeDT:
